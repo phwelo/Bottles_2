514cd616d1106cf134dc5e002b20c504729baab3
Chet <phwelo@gmail.com>
2015-05-22 15:51:37 -0400

    Update phelim.php
    updating connection string

--- phelim.php
+++ phelim.php
@@ -1,7 +1,7 @@
 <?php
 //put the tuff to get the new value from post here
 $phe = $_POST['phe'];
-$con = mysql_connect("localhost","root","ALsk1029");
+$con = mysql_connect("localhost","******","******");
 mysql_select_db("bottles", $con);
 $namequery="UPDATE settings SET TargetPhe = '$phe' WHERE ID=1";
 mysql_query($namequery, $con);
